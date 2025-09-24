8ee05fc575ad3123c8cdef811be0aa200387edea
BGW55 <kiuytpewjd@gmail.com>
2025-09-24 18:05:45 +0900

    프로필 및 키패드 기능 구현

---
+++ settings.gradle.kts
@@ -0,0 +1,25 @@
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "ADP"
+include(":app")
+include(":app:w03")
+include(":app:w04")
