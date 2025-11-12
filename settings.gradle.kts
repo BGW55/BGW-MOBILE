c532ce0be9f1e40225d63871be39b6aee5bc8b33
BGW55 <kiuytpewjd@gmail.com>
2025-11-12 17:14:00 +0900

    test

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -26,9 +26,19 @@ include(":app:w04")
 include(":app:w04:w05")
 include(":app:w05")
 include(":app:w05_buble_game")
+<<<<<<< HEAD
+<<<<<<< HEAD
+include(":app:w06")
+=======
+>>>>>>> f98562e77f37e490e590809f7879cdab36dff37f
+=======
 include(":app:w05")
 include(":app:w05_bubble_game")
 include(":app:w06")
 include(":app:wop-rhythmgame")
+<<<<<<< Updated upstream
 include(":app:w05_buble_game:wop-calender")
 include(":app:wop-calender")
+=======
+>>>>>>> 039be1a54aa65ffcb7e956fcbb7cfe9d11bbf689
+>>>>>>> Stashed changes
